db4bc184c952fe5b2aa399c04ef53519a03d6ef4
Vitaly Zubchevskiy <z-ghost@yandex.ru>
2018-10-18 01:47:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    var body = 'Amazing lightweight webserver using node.js\n';
+    var content_length = body.length;
+    res.writeHead(200, {
+        'Content-Length': content_length,
+        'Content-Type': 'text/plain' });
  
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+    res.end(body);
 });
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 
+server.listen(server_port);
+console.log('Server is running on port ' + server_port);
